88ca1b1ff449c48f3faf808028f5434c441aa098
Freddy <ferreirafreddy39@gmail.com>
2023-12-08 22:09:27 -0300

    no-reply email added

--- src/main/java/com/laboratorio/Lab20/service/EmailServiceImpl.java
+++ src/main/java/com/laboratorio/Lab20/service/EmailServiceImpl.java
@@ -8,18 +8,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements IEmailService{
-
     @Autowired
     private JavaMailSender mailSender;
 
     @Override
     public void sendEmail(EmailDTO emailDTO) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ferreirafreddy39@gmail.com");
         message.setTo("freddysoldev@gmail.com");
         message.setSubject("Nuevo mensaje de " + emailDTO.getName());
         message.setText("Correo de: " + emailDTO.getEmail() + "\n\n" + emailDTO.getMessage());
 
         mailSender.send(message);
+
+        SimpleMailMessage messageToSender = new SimpleMailMessage();
+        messageToSender.setTo(emailDTO.getEmail()); // Usar el email del remitente
+        messageToSender.setSubject("Mensaje enviado con éxito");
+        messageToSender.setText("Su mensaje ha sido enviado con éxito. Gracias por contactarnos.");
+
+        mailSender.send(messageToSender);
     }
 }
