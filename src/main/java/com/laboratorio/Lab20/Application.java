62a4550a627cb1741292b7a9b36c6d73e79547ed
Freddy <ferreirafreddy39@gmail.com>
2023-12-10 21:32:11 -0300

    info encrypted

--- src/main/java/com/laboratorio/Lab20/Application.java
+++ src/main/java/com/laboratorio/Lab20/Application.java
@@ -1,10 +1,12 @@
 package com.laboratorio.Lab20;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
